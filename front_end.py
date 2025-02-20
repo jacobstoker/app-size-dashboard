32edbb8f98597595957b6ca8f136671723af5b86
Jacob Stoker <jacob.stoker42@gmail.com>
2025-02-20 21:30:53 +0000

    Make Dash app a fullscreen stacked area graph

--- front_end.py
+++ front_end.py
@@ -9,11 +9,15 @@ df = pd.read_sql_query('SELECT timestamp, size, section_name FROM elf_sections O
 conn.close()
 
 app = dash.Dash(__name__)
-app.layout = html.Div([
-    dcc.Graph(
-        figure=px.bar(df, x='timestamp', y='size', title="ELF File Size over Time", text="section_name")
-    )
-])
+app.layout = html.Div(
+    children=[
+        dcc.Graph(
+            figure=px.area(df, x='timestamp', y='size', title="ELF File Size over Time", text="section_name", color="section_name"),
+            style={"height": "100vh", "width": "100vw"}
+        )
+    ],
+    style={"height": "100vh", "width": "100vw", "margin": 0, "padding": 0, "overflow": "hidden"}
+)
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", port=8050)
